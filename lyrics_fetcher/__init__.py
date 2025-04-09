6013e43370e37cb4bd8fca82d27e9f8d1522fabf
gabrielbaute <gabrielbaute@gmail.com>
2025-04-09 19:51:21 -0400

    Paquete lyrics fetcher

---
+++ lyrics_fetcher/__init__.py
@@ -0,0 +1,9 @@
+from .core.lrclib_fetcher import LRCLibClient
+from .core.utils import collect_audio_files, get_song_details, replaces
+
+__all__ = [
+    "LRCLibClient",
+    "collect_audio_files",
+    "get_song_details",
+    "replaces"
+]
